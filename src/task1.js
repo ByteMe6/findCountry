18c7c7ea6313ecf3d8d4c3ffe198ac9f0459f720
Yurka2012 <tsapiy.yurka@gmail.com>
2024-10-20 14:47:09 +0300

    rabotaet!

--- src/task1.js
+++ src/task1.js
@@ -1,53 +1,70 @@
-import axios from "axios";
+import axios from 'axios';
 
-let textField = document.querySelector(".countryINfo");
-let findBtn = document.querySelector(".btn");
+let textField = document.querySelector('.countryINfo');
+let findBtn = document.querySelector('.btn');
+let countryImg = document.querySelector('.countryImg');
 
 function getCountry() {
-  let country = document.querySelector(".input").value;
+  let country = document.querySelector('.input').value;
 
-  if(country === "говно"){
-    country = "Russia"
+  if (country === 'говно') {
+    country = 'Russia';
   }
-    if(country === "hentai"){
-    country = "Japan"
+  if (country === 'hentai') {
+    country = 'Japan';
   }
-  axios.get(`https://restcountries.com/v3.1/name/${country}`)
-    .then(function(response) {
+  axios
+    .get(`https://restcountries.com/v3.1/name/${country}`)
+    .then(function (response) {
       let countryInfo = response.data[0];
       let countryName = countryInfo.name;
       let countryCommon = countryName.common;
 
-      let FlagData = response.data[0];
-      let flagUrl = FlagData.flags.png;
-
       let countryRegion = countryInfo.region;
 
-      let languages = countryInfo.languages ? Object.values(countryInfo.languages).join(", ") : "Информация о языках отсутствует";
+      let languages = countryInfo.languages
+        ? Object.values(countryInfo.languages).join(', ')
+        : 'Информация о языках отсутствует';
+      // ? - если
 
-      let capital = countryInfo.capital ? countryInfo.capital[0] : "Информация о столице отсутствует";
+      let capital = countryInfo.capital
+        ? countryInfo.capital[0]
+        : 'Информация о столице отсутствует';
+
+              let flagEmoji = countryInfo.flag;
+              console.log(flagEmoji)
+              if(flagEmoji === "🇷🇺"){
+                flagEmoji = "💩"
+              }
+
 
       // Очищаем предыдущее содержимое
       textField.innerHTML = '';
 
       // Создаем элемент для текстовой информации
-      let textInfo = document.createElement('p');
-      textInfo.textContent = `Запрос на страну: ${countryCommon}, Материк: ${countryRegion}, Официальный язык(и): ${languages}, Столица: ${capital}`;
-      textField.appendChild(textInfo);
+      // let textInfo = document.createElement('p');
+      textField.textContent = `Запрос на страну: ${countryCommon} ${flagEmoji}, Материк: ${countryRegion}, Официальный язык(и): ${languages}, Столица: ${capital}`;
+      // textField.appendChild(textInfo);
 
       // Создаем и добавляем изображение флага
+
+      let FlagData = response.data[0];
+      let flagUrl = FlagData.flags.png;
+
+      countryImg.innerHTML = "";
+
       let flagImg = document.createElement('img');
       flagImg.src = flagUrl;
       flagImg.alt = `Флаг ${countryCommon}`;
-      flagImg.style.maxWidth = '200px'; 
-      textField.appendChild(flagImg);
+      flagImg.style.maxWidth = '200px';
+      countryImg.appendChild(flagImg);
 
       console.log(response);
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.error(error);
       textField.textContent = `Вы ввели страну неправильно, пишите на английском, полное слово`;
     });
 }
 
-findBtn.addEventListener("click", getCountry);
+findBtn.addEventListener('click', getCountry);
